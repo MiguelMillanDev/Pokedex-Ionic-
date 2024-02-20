76f45a9038e3492f894a85463c1093bfe9b9238f
SukunaDevs <143455824+SukunaDevs@users.noreply.github.com>
2024-02-20 21:52:48 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'HITO2-T2-Pokedex',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
